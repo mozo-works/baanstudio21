761fc814fc01a08aed1ae97e7a58db979f3b4c83
yeongtaek, Hong <mozodev@users.noreply.github.com>
2021-05-04 00:11:54 +0900

    프로젝트 상세 화면 작업 유형 클릭 시 스크롤 세로 위치를 조정합니다.

--- web/themes/custom/bs21/src/js/bs21.script.js
+++ web/themes/custom/bs21/src/js/bs21.script.js
@@ -21,16 +21,25 @@ import 'bootstrap/dist/js/bootstrap.bundle.js'
   $(function () {
     $('#try-search').on('click', e => {
       $('#search-block').removeClass('d-none').addClass('position-absolute');
-    })
-    $('#nav-work-types a').on('click', e => {
-      $('#nav-work-types a').removeClass('active');
-      $(e.currentTarget).addClass('active');
     });
+
+    if ($('#nav-work-types').length > 0) {
+      // 클릭 시 active.
+      $('#nav-work-types a').on('click', e => {
+        $('#nav-work-types a').removeClass('active');
+        $(e.currentTarget).addClass('active');
+        e.preventDefault();
+        let target = $($(e.currentTarget).attr('href')).offset().top - $('#nav-work-types').offset().top;
+        window.scrollTo(0, target);
+      });
+    }
+
     let footerBottom = window.screen.height - $('#site-footer').offset().top;
     if (footerBottom > 70) {
       $('#site-footer').css('marginTop', footerBottom - 250);
       $('#btn-top').addClass('d-none');
     }
+
   });
 
 })(jQuery, Drupal);
