3b3965fbb2ccd51ccfee990c1953cfe8e65a294b
yeongtaek, Hong <mozodev@users.noreply.github.com>
2021-05-10 16:07:02 +0900

    프로젝트 아카이브 목록 화면에서 레이아웃 우측 마진 40px으로 지정하고 이미지와 텍스트 사이 간격을 추가합니다.

--- web/themes/custom/bs21/src/js/bs21.script.js
+++ web/themes/custom/bs21/src/js/bs21.script.js
@@ -1,54 +1,66 @@
 import 'bootstrap/dist/js/bootstrap.bundle.js'
-import vars from '../sass/bs21.style.scss'
+import vars from '../sass/export.scss'
 
-console.log(vars);
+let mediaDown = (breakpoint) => {
+  // console.log(breakpoint, vars[breakpoint], window.matchMedia("screen and (max-width:" + vars[breakpoint] + ")").matches)
+  return window.matchMedia("(max-width:" + vars[breakpoint] + ")").matches
+}
 
 (function ($, Drupal) {
-  'use strict';
+  'use strict'
   Drupal.behaviors.scrollToTop = {
     attach: function (context) {
       $('#btn-top', context).once('btn-top').on('click', () => {
-        $("html, body").animate({ scrollTop: "10px" });
+        $("html, body").animate({ scrollTop: "10px" })
       })
     }
-  };
+  }
   Drupal.behaviors.setWidthWorkTypeNav = {
     attach: function (context) {
       if ($('.project--full').length > 0) {
-        let col = $('.project--full .col-md-1');
-        $('#nav-work-types').width(col.width());
+        let col = $('.project--full .col-md-1')
+        $('#nav-work-types').width(col.width())
       }
     }
   }
 
   Drupal.behaviors.fixByScreen = {
     attach: function (context) {
-      console.log(Drupal);
+      if (mediaDown('sm')) {
+        $('#search-input').css('maxWidth', '140px')
+      }
+      if (mediaDown('xl')) {
+        $('#search-input').css('maxWidth', '70px')
+      }
+      if (mediaDown('xxl')) {
+        $('.card--project .project--teaser span').css('marginTop', '9px')
+        $('#search-input').css('maxWidth', '130px')
+      }
     }
-  };
+  }
 
   $(function () {
     $('#try-search').on('click', e => {
-      $('#search-block').removeClass('d-none').addClass('position-absolute');
-    });
+      $('#search-block').removeClass('d-none').addClass('position-absolute')
+    })
 
     if ($('#nav-work-types').length > 0) {
       // 클릭 시 active.
       $('#nav-work-types a').on('click', e => {
-        $('#nav-work-types a').removeClass('active');
-        $(e.currentTarget).addClass('active');
-        e.preventDefault();
-        let target = $($(e.currentTarget).attr('href')).offset().top - $('#nav-work-types').offset().top;
-        window.scrollTo(0, target);
-      });
+        $('#nav-work-types a').removeClass('active')
+        $(e.currentTarget).addClass('active')
+        e.preventDefault()
+        let target = $($(e.currentTarget).attr('href')).offset().top - $('#nav-work-types').offset().top
+        window.scrollTo(0, target)
+      })
     }
 
-    let footerBottom = window.screen.height - $('#site-footer').offset().top;
+    let footerBottom = window.screen.height - $('#site-footer').offset().top
     if (footerBottom > 70) {
-      $('#site-footer').css('marginTop', footerBottom - 250);
-      $('#btn-top').addClass('d-none');
+      $('#site-footer').css('marginTop', footerBottom - 250)
+      $('#btn-top').addClass('d-none')
     }
 
-  });
+  })
 
-})(jQuery, Drupal);
+})(jQuery, Drupal)
