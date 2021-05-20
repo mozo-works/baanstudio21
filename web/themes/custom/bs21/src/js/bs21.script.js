097034afa2524fff8add43157d777cf2a268811e
yeongtaek, Hong <mozodev@users.noreply.github.com>
2021-05-20 16:34:16 +0900

    모바일 메뉴 동작을 추가합니다.

--- web/themes/custom/bs21/src/js/bs21.script.js
+++ web/themes/custom/bs21/src/js/bs21.script.js
@@ -2,7 +2,6 @@ import 'bootstrap/dist/js/bootstrap.bundle.js'
 import vars from '../sass/export.scss'
 
 let mediaDown = (breakpoint) => {
-  // console.log(breakpoint, vars[breakpoint], window.matchMedia("screen and (max-width:" + vars[breakpoint] + ")").matches)
   return window.matchMedia("(max-width:" + vars[breakpoint] + ")").matches
 }
 
@@ -10,41 +9,79 @@ let mediaDown = (breakpoint) => {
   'use strict'
   Drupal.behaviors.scrollToTop = {
     attach: function (context) {
-      $('#btn-top', context).once('btn-top').on('click', () => {
-        $("html, body").animate({ scrollTop: "10px" })
+      $('#btn-top, #navbar--btn-top', context).once('btn-top').on('click', () => {
+        $("html, body, #navbarSupportedContent").animate({ scrollTop: "10px" })
       })
     }
   }
   Drupal.behaviors.setWidthWorkTypeNav = {
-    attach: function (context) {
+    attach: function () {
       if ($('.project--full').length > 0) {
         let col = $('.project--full .col-md-1')
         $('#nav-work-types').width(col.width())
       }
     }
   }
-
   Drupal.behaviors.fixByScreen = {
-    attach: function (context) {
-      if (mediaDown('sm')) {
-        $('#search-input').css('maxWidth', '140px')
-        $('#global-nav .navbar-nav.row.w-100').height($(window).height())
-          .append('<li class="position-absolute" style="bottom: 90px;">© Baan Studio</a>');
-        if ($('.projects-browse').length > 0) {
-          $('.projects-browse .row pe-3').hide();
+    attach: function (context, settings) {
+      $('body', context).once('responsive').each(() => {
+        if (mediaDown('md')) {
+          initNavbar(settings)
+          $('.navbar-nav a').on('click', function(e){
+            e.preventDefault()
+            activeNavbarItem(e.target)
+          })
+          if ($('.projects-browse').length > 0) {
+            $('.projects-browse .row.pe-3').hide()
+          }
         }
-      }
-      if (mediaDown('xl')) {
-        $('#search-input').css('maxWidth', '70px')
-      }
-      if (mediaDown('xxl')) {
-        console.log('xxl');
-        $('.card--project .project--teaser span').css('marginTop', '9px')
-        $('#search-input').css('maxWidth', '130px')
-      }
-      if (mediaDown('mbp')) {
-        $('#search-input').css('maxWidth', '140px')
-      }
+        if (mediaDown('xl')) {
+          $('#search-input').css('maxWidth', '70px')
+        }
+        if (mediaDown('xxl')) {
+          $('.card--project .project--teaser span').css('marginTop', '9px')
+          $('#search-input').css('maxWidth', '130px')
+        }
+        if (mediaDown('mbp')) {
+          $('#search-input').css('maxWidth', '140px')
+        }
+      })
+    }
+  }
+  function initNavbar(settings) {
+    console.log('initNavbar')
+    $('#search-input').css('maxWidth', '140px')
+    $('#global-nav .container-fluid, #global-nav .container-fluid .navbar-nav').removeClass('row');
+    $('#navbarSupportedContent').removeClass('ps-2')
+    $('#navbarSupportedContent li a').removeClass('ms-2')
+    $('#navbarInner').css('minHeight', $(window).height() + 'px')
+    // 툴바있는 경우 하단 푸터 위치 조정.
+    if (settings.toolbar) {
+      $('#navbarFooter').css('bottom', '+=40')
+    }
+    $('#navbarInnerContainer').removeClass('d-none')
+  }
+  function resetNavbar() {
+    $('body').removeClass('noscroll');
+    let activeClass = 'text-dark border-bottom pb-1'
+    $('#navbarSupportedContent li a').removeClass(activeClass)
+      .closest('.nav-item').removeClass('text-start')
+    $('#navbarInnerContent .inner-content').addClass('d-none')
+  }
+  function activeNavbarItem(target) {
+    resetNavbar()
+    $('body').addClass('noscroll')
+    $('#navbarSupportedContent').css({
+      'height': $(window).height() - 44 + 'px',
+      'overflowY': 'scroll'
+    })
+    let activeClass = 'text-dark border-bottom pb-1'
+    let targetId = $(target).data('target');
+    $(target).addClass(activeClass)
+      .closest('.nav-item').addClass('text-start')
+    $('#navbarFooter').removeClass('position-absolute')
+    if (targetId) {
+      $('#content--' + targetId).removeClass('d-none')
     }
   }
 
@@ -67,8 +104,12 @@ let mediaDown = (breakpoint) => {
     let footerBottom = window.screen.height - $('#site-footer').offset().top
     if (footerBottom > 70 && footerBottom - 250 > 0) {
       $('#site-footer').css('marginTop', footerBottom - 250)
-      $('#btn-top').addClass('d-none')
+      $('#btn-top, #navbar--btn-top').addClass('d-none')
     }
+
+    $('#navbarSupportedContent').on('hide.bs.collapse', e => {
+      resetNavbar()
+    })
 
   })
 
